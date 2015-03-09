a4829cc8693e5c9b4ed7915550a72324b0557a33
Adrian Perez de Castro <aperez@igalia.com>
2015-03-09 19:40:28 +0100

    tools/hipack-parse: New tool to exercise the parser

--- Makefile
+++ Makefile
@@ -9,9 +9,17 @@ hipack = ${hipack_PATH}/libhipack.a
 hipack: ${hipack}
 hipack-clean:
 	${RM} ${hipack} ${hipack_OBJS}
+	${RM} ${hipack_PATH}/tools/*.o ${hipack_PATH}/tools/hipack-parse
 
 ${hipack_OBJS}: ${hipack_PATH}/hipack.h
 ${hipack}: ${hipack_OBJS}
 	${AR} rcu ${hipack} ${hipack_OBJS}
 
-.PHONY: hipack hipack-objs
+hipack-tools: \
+	${hipack_PATH}/tools/hipack-parse
+
+${hipack_PATH}/tools/hipack-parse: \
+	${hipack_PATH}/tools/hipack-parse.o ${hipack}
+
+
+.PHONY: hipack hipack-objs hipack-tools
---
+++ tools/hipack-parse.c
@@ -0,0 +1,39 @@
+/*
+ * hipack-parse.c
+ * Copyright (C) 2015 Adrian Perez <aperez@igalia.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include "../hipack.h"
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+
+int
+main (int argc, const char *argv[])
+{
+    if (argc != 2) {
+        fprintf (stderr, "Usage: %s PATH\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    FILE *fp = fopen (argv[1], "rb");
+    if (!fp) {
+        fprintf (stderr, "%s: Cannot open '%s' (%s)\n",
+                 argv[0], argv[1], strerror (errno));
+        return EXIT_FAILURE;
+    }
+
+    int retcode = EXIT_SUCCESS;
+    hipack_dict_t *message = hipack_read (fp);
+    if (!message) {
+        retcode = EXIT_FAILURE;
+    }
+    hipack_dict_free (message);
+
+    fclose (fp);
+    return retcode;
+}
+
